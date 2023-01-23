c14a898c212a5e76dba67918c71c86109b1bc50c
maximbabaiev <116840964+maximbabaiev@users.noreply.github.com>
2023-01-23 20:37:21 +0200

    Update settings.py

--- store/store/settings.py
+++ store/store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-(i7a4h&+0x0!=ajf!djgcpu7#c0&c+x1p379e%&4w#hqik*m2$'
+
 
 
 
